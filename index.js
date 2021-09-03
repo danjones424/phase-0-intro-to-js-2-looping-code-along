37d5fbde64196b6278b4b3b29f5ebfbc2470e405
Daniel <daniel.jones424@gmail.com>
2021-09-03 19:03:16 -0400

    Copmleted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards (array, event){
+    const newArray = []
+    let i = "";
+    for(i = 0; i < array.length; i++){
+      let x = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+       newArray.push(x)
+    }
+    return newArray;
+}
+
+function countDown (number){
+    let a = number;
+    while (a >= 0){
+         console.log(a)
+         a--;
+    }
+}
